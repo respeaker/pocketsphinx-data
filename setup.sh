d5200ad900bafaa393042794e39cf8f27f93fd4d
Yihui Xiong <yihui_xiong@163.com>
2016-09-21 14:05:12 +0800

    fixed setup.sh, update description

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-CURRENT_FILE_DIR="$( cd "$( dirname "$[0]" )" && pwd )"
+CURRENT_FILE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-export POCKETSPHINX_DATA="$(CURRENT_FILE_DIR)"
+export POCKETSPHINX_DATA="$CURRENT_FILE_DIR"
